3e68890065d7f7106ca9ab3d153023e1f6afe222
KIM_MINSU <kimminsu65772@gmail.com>
2024-11-08 01:01:14 +0900

    수정내용이 설정창에 반영되도록 수정

---
+++ src/configscreens/index.js
@@ -0,0 +1,10 @@
+import ConfigScreen from "./ConfigScreen";
+import TermsOfService from "./TermsOfService";
+import NoticeList from "./NoticeList";
+import NoticeScreen from "./NoticeScreen";
+import UserProfile from "./UserProfile";
+import Name from "./Name";
+import Birth from "./Birth";
+import PhoneNumber from "./PhoneNumber";
+
+export { ConfigScreen, TermsOfService, NoticeList, NoticeScreen, UserProfile, Name, Birth, PhoneNumber };
